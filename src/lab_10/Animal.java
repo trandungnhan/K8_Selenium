51855fdd1feee7b7d0e890064a4c88a00cba7d4b
Tran Dung Nhan <trandungnhan89@gmail.com>
2022-06-23 21:15:05 +0700

    lab 10

---
+++ src/lab_10/Animal.java
@@ -0,0 +1,11 @@
+package lab_10;
+/*
+SIMPLE! Do the LAB 8.2 again with abstraction! (speed method should be abstract method)
+* */
+
+public abstract class Animal {
+
+    public abstract int getSpeed();
+    public abstract String getName();
+
+}
---
+++ src/lab_10/Horse.java
@@ -0,0 +1,17 @@
+package lab_10;
+
+import java.security.SecureRandom;
+
+public class Horse extends Animal {
+
+    @Override
+    public int getSpeed() {
+        int randomNumber = new SecureRandom().nextInt(75);
+        return randomNumber;
+    }
+
+    @Override
+    public String getName() {
+        return "Horse";
+    }
+}
